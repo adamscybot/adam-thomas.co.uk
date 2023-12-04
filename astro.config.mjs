ebe2b728bf97212982f3b5c0633cd0bf44fd265a
adamscybot <adam@adam-thomas.co.uk>
2023-12-04 19:44:09 +0000

    New personal site based on astro

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import pandacss from '@pandacss/astro';
+
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
+export default defineConfig({
+  prefetch: true,
+  site: 'https://adam-thomas.co.uk',
+  integrations: [mdx(), pandacss(), sitemap()],
+  output: "static",
+  adapter: vercel({ 
+	webAnalytics: {
+		enabled: true,
+  	}
+})});
---
+++ panda.config.ts
@@ -0,0 +1,95 @@
+import { defineConfig, defineTextStyles } from "@pandacss/dev";
+
+export const textStyles = defineTextStyles({
+    pageTitle: {
+      description: 'Large display text style for feature elements',
+      value: {
+        fontFamily: 'inter',
+        fontWeight: '700',
+        fontSize: '3rem',
+        lineHeight: '1.4',
+      }
+    },
+    tagline: {
+      description: 'Tag line for the large display text',
+      value: {
+        fontFamily: 'inter',
+        fontWeight: '400',
+        fontSize: '1.1rem',
+        lineHeight: '1.5',
+      }
+    },
+    body: {
+      description: 'Tag line for the large display text',
+      value: {
+        fontFamily: 'inter',
+        fontWeight: '400',
+        fontSize: '0.95rem',
+        lineHeight: '1.7',
+      }
+    },
+    headerLink: {
+      description: 'Main header navigation links',
+      value: {
+        fontFamily: 'inter',
+        fontWeight: '700',
+        fontSize: '1rem',
+        lineHeight: '1.35',
+        textTransform: 'uppercase',
+      }
+    },
+    link: {
+      description: 'Inline links',
+      value: {
+        fontFamily: 'inter',
+        fontWeight: '400',
+        textDecoration: 'underline',
+        _hover: {
+          textDecoration: 'none'
+        }
+      }
+    },
+    header: {
+      description: 'Inline links',
+      value: {
+        fontFamily: 'inter',
+        fontWeight: '600',
+        fontSize: '1.05rem',
+        textTransform: 'uppercase'
+      }
+    }
+})
+
+
+
+export default defineConfig({
+  preflight: true,
+  include: ['./src/**/*.{ts,tsx,js,jsx,astro}', './pages/**/*.{ts,tsx,js,jsx,astro}'],
+  exclude: [],
+  theme: {
+    extend: {
+      textStyles,
+      tokens: {
+        fonts: {
+          inter: {value: "'Inter Variable', sans-serif"}
+        }
+      },
+      semanticTokens: {
+        colors: {
+          bg: { value: '{colors.slate.950}' },
+          text: {
+            pageTitle: { value: '{colors.slate.200}'},
+            header: { value: '{colors.slate.200}'},
+            tagline: { value: '{colors.slate.300}'},
+            body: { value: '{colors.slate.400}'},
+          }
+        }
+      },
+      breakpoints: {
+        '2xl': '1450px'
+      }
+    },
+
+  },
+  outdir: "src/styled-system",
+});
